357d8e843cb74ed0ce585e0567ff08fa7ca3d630
unknown <90450@hall-net.local>
2021-03-12 17:25:27 +0900

    giron-noine

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header ({children}) {
+    return(
+        <div>
+            <h1>Naoya Sato - noine</h1>
+            {children}
+        </div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+/*import { Link } from "gatsby"*/
+import Header from "../components/header"
+
+export default function Home() {
+  return (
+    <Header>
+    <Layout>
+      <p>Hello world!</p>
+      
+    </Layout>
+    </Header>
+  );
+}
