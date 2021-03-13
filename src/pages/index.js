aed780f4f8e1e8436b8d82b6d50f781382647813
giron-noine <noineniya@gmail.com>
2021-03-13 15:55:50 +0900

    20210313

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,61 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { css } from "@emotion/react"
+import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
-/*import { Link } from "gatsby"*/
+
 import Header from "../components/header"
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
   return (
     <Header>
     <Layout>
-      <h1>Amazing Pandas Rating Things</h1>
       <div>
-      <img
-          src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-          alt="Group of pandas eating bamboo"
-        />
+        <h1 css={css`
+          display: inline-block;
+          border-bottom: 1px solid;
+        `}>
+          Amazing Pandas Rating Things
+        </h1>
+          <h4>{data.allMarkdownRemark.totalCount} Post</h4>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+              `}>
+              {node.frontmatter.title}{" "}
+              <span
+                css={css`
+                color: #bbb;
+              `}>
+              - {node.frontmatter.data}
+              </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </div>
+          ))}
       </div>
     </Layout>
     </Header>
-  );
+  )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            data(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
