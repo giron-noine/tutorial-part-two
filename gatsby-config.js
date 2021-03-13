33d263ba22f99a94f084e06472d9773c696b6ca3
giron-noine <noineniya@gmail.com>
2021-03-13 14:59:55 +0900

    20210313

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,14 @@ module.exports = {
 
   plugins: [
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+      `gatsby-plugin-emotion`,
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
